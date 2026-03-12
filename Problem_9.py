65257dd48010ab65cc5bb794006eb793f749c08e
Rohit Patil <itzrohitpatil08@gmail.com>
2026-03-12 16:06:21 +0530

    Solved Problem 9

---
+++ Problem_9.py
@@ -0,0 +1,10 @@
+#Problem 9
+import datetime
+
+def solution_triplet():
+    for a in range(1, 1000):
+        for b in range(a, 1000):
+            for c in range(b, 1000):
+                if a + b + c == 1000 and a**2 + b**2 == c**2:
+                    return a * b * c
+print(solution_triplet())
