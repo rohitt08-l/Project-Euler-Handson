edb2d223e6f3f7c4693695bd647e9b4143804186
Rohit Patil <itzrohitpatil08@gmail.com>
2026-08-23 22:40:42 +0530

    Update Problem_15.py

--- Problem_15.py
+++ Problem_15.py
@@ -1,6 +1,6 @@
 #Problem 15
 import math
 #Here we are uding comb  function of math library which will return us the possible combination
-#format - comb(a,b) , where a=no.of steps , b=no.of rows,columns
+#format - comb(a,b) ,where a=no.of steps and b=no.of rows,columns
 #Here we need to calucualte for 20 by 20  matrix so b= 20 and routes =40
 print(math.comb(40,20))
